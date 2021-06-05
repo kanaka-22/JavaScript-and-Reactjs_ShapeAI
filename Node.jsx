2bca55c09fd748821367f3e51420699761309ace
Kanakaprabha S <61780331+kanaka-22@users.noreply.github.com>
2021-06-05 15:01:52 +0530

    Add files via upload

---
+++ Node.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Node() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>
+        This was an amazing Bootcamp taken up by Shaurya Sir we covered
+        everything from Scratch including Javascript react.js,HTML.
+      </p>
+    </div>
+  );
+}
+
+export default Node;
